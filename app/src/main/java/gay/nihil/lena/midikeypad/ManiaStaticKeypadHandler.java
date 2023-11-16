9dc9ca4c457ed3f699a15c2e5a1b6d8c4fa19a6e
Lena <me@lena.place>
2023-11-16 09:55:10 +0100

    fix tracking issue for held fingers

--- app/src/main/java/gay/nihil/lena/midikeypad/ManiaStaticKeypadHandler.java
+++ app/src/main/java/gay/nihil/lena/midikeypad/ManiaStaticKeypadHandler.java
@@ -4,13 +4,22 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class ManiaStaticKeypadHandler extends KeypadHandler {
+    int down0;
+    int down1;
+    int down2;
+    int down3;
     public ManiaStaticKeypadHandler(View view) {
         super(view);
+        this.down0 = -1;
+        this.down1 = -1;
+        this.down2 = -1;
+        this.down3 = -1;
     }
 
     @Override
     public KeypadData HandleMotionEvent(MotionEvent event) {
-        float x = event.getX(event.getActionIndex());
+        int index = event.getActionIndex();
+        float x = event.getX(index);
         float width = view.getWidth();
         int code = 60;
         if (x > (width / 4) && x < (width / 2)) {
@@ -23,9 +32,72 @@ public class ManiaStaticKeypadHandler extends KeypadHandler {
 
         int a = event.getActionMasked();
         if (a == MotionEvent.ACTION_DOWN || a == MotionEvent.ACTION_POINTER_DOWN) {
+            switch (code) {
+                case 60:
+                    if (down0 < 0) {
+                        down0 = index;
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                    break;
+                case 61:
+                    if (down1 < 0) {
+                        down1 = index;
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                    break;
+                case 62:
+                    if (down2 < 0) {
+                        down2 = index;
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                    break;
+                case 63:
+                    if (down3 < 0) {
+                        down3 = index;
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                    break;
+                default:
+                    return new KeypadData(false, false, 0);
+            }
             return new KeypadData(true, true, code);
         } else if (a == MotionEvent.ACTION_UP || a == MotionEvent.ACTION_POINTER_UP) {
-            return new KeypadData(true, false, code);
+            switch (code) {
+                case 60:
+                    if (index == down0) {
+                        down0 = -1;
+                        return new KeypadData(true, false, code);
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                case 61:
+                    if (index == down1) {
+                        down1 = -1;
+                        return new KeypadData(true, false, code);
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                case 62:
+                    if (index == down2) {
+                        down2 = -1;
+                        return new KeypadData(true, false, code);
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                case 63:
+                    if (index == down3) {
+                        down3 = -1;
+                        return new KeypadData(true, false, code);
+                    } else {
+                        return new KeypadData(false, false, 0);
+                    }
+                default:
+                    return new KeypadData(false, false, 0);
+            }
         } else {
             return new KeypadData(false, false, 0);
         }
--- app/src/main/java/gay/nihil/lena/midikeypad/OsuStaticKeypadHandler.java
+++ app/src/main/java/gay/nihil/lena/midikeypad/OsuStaticKeypadHandler.java
@@ -5,22 +5,54 @@ import android.view.View;
 
 public class OsuStaticKeypadHandler extends KeypadHandler {
 
+    int down0;
+    int down1;
     public OsuStaticKeypadHandler(View view) {
         super(view);
+        this.down0 = -1;
+        this.down1 = -1;
     }
 
     @Override
     public KeypadData HandleMotionEvent(MotionEvent event) {
         int code = 60;
-        if (event.getX(event.getActionIndex()) > (view.getWidth() / 2)) {
+        int index = event.getActionIndex();
+        if (event.getX(index) > (view.getWidth() / 2)) {
             code = 61;
         }
 
         int a = event.getActionMasked();
         if (a == MotionEvent.ACTION_DOWN || a == MotionEvent.ACTION_POINTER_DOWN) {
+            if (code == 60) {
+                if (down0 < 0) {
+                    down0 = index;
+                } else {
+                    return new KeypadData(false, false, 0);
+                }
+            } else {
+                if (down1 < 0) {
+                    down1 = index;
+                } else {
+                    return new KeypadData(false, false, 0);
+                }
+            }
             return new KeypadData(true, true, code);
         } else if (a == MotionEvent.ACTION_UP || a == MotionEvent.ACTION_POINTER_UP) {
-            return new KeypadData(true, false, code);
+            if (code == 60) {
+                if (index == down0) {
+                    down0 = -1;
+                    return new KeypadData(true, false, code);
+                } else {
+                    return new KeypadData(false, false, 0);
+                }
+            } else {
+                if (index == down1) {
+                    down1 = -1;
+                    return new KeypadData(true, false, code);
+                } else {
+                    return new KeypadData(false, false, 0);
+                }
+            }
         } else {
             return new KeypadData(false, false, 0);
         }
